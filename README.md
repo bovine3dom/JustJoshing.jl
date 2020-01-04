ec1e9971447e0a935d4548071578bb1a8bea99e7
Oliver Blanthorn <freedom4cows@gmail.com>
2020-01-04 14:15:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projects from Joshi's Concepts and Practice of Financial Mathematics
+
+I thought I should probably brush up on my SDEs as it has been a little while.
+
+Initially I'll do all of this in Julia because it's better. I might try Python and C++ later.
---
+++ p1_black_scholes_vanilla/black_scholes_vanilla.jl
@@ -0,0 +1,71 @@
+#!/usr/bin/env julia
+
+# Best to run this with JULIA_NUM_THREADS=$(nproc) and julia 1.3
+
+# Notation: if f is a function of x, fv is one realisation of that function
+
+using Distributions: Normal, cdf
+
+using ForwardDiff: derivative
+
+import Plots
+Plots.unicodeplots()
+
+N(x) = cdf(Normal(),x)
+
+# Strike price discounted by time value of money
+PV(t,T,K,r) = K*exp(-r*(T-t))
+
+d_1(S_t,t,T,K,r,σ) = 1/(σ*√(T-t)) * (log(S_t/K) + (r + σ^2/2)*(T-t))
+d_2(S_t,t,T,K,r,σ) = d_1(S_t,t,T,K,r,σ) - σ*√(T-t)
+
+
+# Price of a call option (i.e. the holder has the right to buy stock at maturity)
+# S_t = current stock (underlying) price
+# t = current time (time since option created)
+# T = time of maturity
+# K = the price at which the stock may be bought at maturity
+# r = the continuously compounded risk-free rate
+# σ = volatility of the underlying (standard deviation per root unit of time)
+function C(S_t,t; T=1, K=1, r=0.5, σ=0.5)
+    d_1v = d_1(S_t,t,T,K,r,σ)
+    d_2v = d_2(S_t,t,T,K,r,σ)
+    PVv = PV(t,T,K,r)
+    #N(d_1v)*S_t - N(d_2v)*PVv
+    S_t - PVv
+    # Even without randomness the C >= S_t - Ke(-rT) isn't true
+    # e(-r(T-t))  >= e(-rT)
+end
+
+# Price of a put option (i.e. holder may sell at maturity)
+function P(S_t,t; T=1, K=1, r=0.5, σ=0.5)
+    PVv = PV(t,T,K,r)
+    PVv - S_t + C(S_t,t, T=T, K=K, r=r, σ=σ)
+end
+
+
+# Tests
+#
+# Price of call option monotonically decreases with strike price
+@assert all(derivative.(x->C(1,0.5;K=x),rand(100)*10) .< 0)
+
+#
+# Call option should be between S and S - Ke(-rT) for all inputs
+#
+# (if call option is more expensive than stock, it is clearly better
+# to just buy the stock; if it is cheaper than the difference between
+# the current value of the stock and the discounted strike price, we
+# can arbitrage by selling the stock now, buying the option, putting the
+# strike price in a risk free bond, and then exercising the option
+# (so we end up with the stock back and some profit))
+#
+let (S_t,t,K,r,σ) = rand(5), T = t + rand()
+    # The following is true
+    println(S_t >= C(S_t,t;T=T,K=K,r=r,σ=σ))
+
+    # The following is rarely true, so we have a bug
+    println(C(S_t,t;T=T,K=K,r=r,σ=σ) >= S_t - K*exp(-r*T))
+end
+
+#
+# Price of call option should monotonically increase with volatility
