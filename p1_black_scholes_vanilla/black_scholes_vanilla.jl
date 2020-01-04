d08a846d4a5550984e547081794335fbb8b409a8
Oliver Blanthorn <freedom4cows@gmail.com>
2020-01-04 17:47:12 +0000

    Add Brownian motion
    Also increase number of tests on assertions, increase prices to
    about 100 as is tradition

--- p1_black_scholes_vanilla/black_scholes_vanilla.jl
+++ p1_black_scholes_vanilla/black_scholes_vanilla.jl
@@ -24,23 +24,22 @@ d_2(S,t,T,K,r,σ) = (log(S/K) + (r - (σ^2)/2)*(T-t)) / (σ*√(T-t))
 # K = the price at which the stock may be bought at maturity
 # r = the continuously compounded risk-free rate
 # σ = volatility of the underlying (standard deviation per root unit of time)
-function C(S,t; T=1, K=1, r=0.5, σ=0.5)
+function C(S,t; T=1, K=100, r=0.02, σ=0.5)
     d_1v = d_1(S,t,T,K,r,σ)
     d_2v = d_2(S,t,T,K,r,σ)
     N(d_1v)*S - N(d_2v)*K*exp(-r*(T-t))
 end
 
 # Price of a put option (i.e. holder may sell at maturity)
-function P(S,t; T=1, K=1, r=0.5, σ=0.5)
-    PVv = PV(t,T,K,r)
-    PVv - S + C(S,t, T=T, K=K, r=r, σ=σ)
+function P(S,t; T=1, K=100, r=0.02, σ=0.5)
+    K*exp(-r*(T-t)) - S + C(S,t, T=T, K=K, r=r, σ=σ)
 end
 
 
 # Tests
-#
+
 # Price of call option monotonically decreases with strike price
-@assert all(derivative.(x->C(1,0.5;K=x),rand(100)*10) .< 0)
+@assert all(derivative.(x->C(1,0.5;K=x),rand(100_000)*10) .<= 0)
 
 #
 # Call option should be between S and S - Ke(-rT) for all inputs
@@ -56,12 +55,26 @@ let (S,t,K,r,σ) = rand(5), T = t + rand()
     # The following is true
     println(S >= C(S,t;T=T,K=K,r=r,σ=σ))
 
-    # The following is rarely true, so we have a bug
+    # The following is rarely true, so we have a bug (either in the code or my understanding)
     println(C(S,t;T=T,K=K,r=r,σ=σ) >= S - K*exp(-r*T))
 end
 
 # This should look like Figure 5.3, page 121 in Joshi (it does)
+# NB: looks like Joshi's graph is mislabelled - should be spot price
 p = Plots.plot(); for t in 0:0.2499:1; Plots.plot!(p,x->C(x,t;K=100,σ=0.3,r=0),60:140); end; p
 
-#
+Plots.plot(x->C(1,x),0:0.001:0.9999) # Value of at-the-money approaches zero as time approaches maturity (Fig 2.1, page 35)
+
+Plots.plot(x->C(0.5,0;σ=x),0:0.01:0.35) # Call options with volatile underlyings are more expensive (Fig 3.8, page 66)
+
+
 # Price of call option should monotonically increase with volatility
+@assert all(derivative.(x->C(1,0.5;σ=x),rand(100_000).*0.99) .>= 0)
+
+
+# Validation via Monte Carlo
+# Brownian motion
+B(T;B0=100,r=0.02,d=-0.06,σ=0.5) = B0*exp((r-d)*T-0.5*σ^2*T+σ*√T*rand(Normal()))
+
+# Simulated stock price
+Plots.plot(x->B(x,σ=0.02),0:0.01:1)
