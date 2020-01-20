b0b434433c7ed9b2f178d3a43b90149b8029e0ef
Oliver Blanthorn <freedom4cows@gmail.com>
2020-01-20 16:53:28 +0000

    Volatility 'fixed': errors probably just to floating points

--- p1_black_scholes_vanilla/black_scholes_vanilla.jl
+++ p1_black_scholes_vanilla/black_scholes_vanilla.jl
@@ -64,11 +64,11 @@ p = Plots.plot(); for t in 0:0.2499:1; Plots.plot!(p,x->C(x,t;K=100,σ=0.3,r=0),
 
 Plots.plot(x->C(1,x),0:0.001:0.9999) # Value of at-the-money approaches zero as time approaches maturity (Fig 2.1, page 35)
 
-Plots.plot(x->C(0.5,0;σ=x),0:0.01:0.35) # Call options with volatile underlyings are more expensive (Fig 3.8, page 66)
+Plots.plot(x->C(0.5,0;σ=x),0:0.01:1) # Call options with volatile underlyings are more expensive (Fig 3.8, page 66)
 
 
 # Price of call option should monotonically increase with volatility
-@assert all(derivative.(x->C(1,0.5;σ=x),rand(100_000).*0.99) .>= 0)
+@assert all(derivative.(x->C(1,0.5;σ=x),rand(1000)) .>= 0)
 
 
 # Validation via Monte Carlo
