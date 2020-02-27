cb4e09275a7e8f9033c5f0c192bc77a913763922
Oliver Blanthorn <freedom4cows@gmail.com>
2020-02-27 18:09:02 +0000

    Start moving to a normal Julia project structure

---
+++ just_joshing.jl
@@ -0,0 +1,5 @@
+module just_joshing
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using just_joshing
+using Test
+
+@testset "just_joshing.jl" begin
+    # Write your own tests here.
+end
