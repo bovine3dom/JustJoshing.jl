b391eaf031819abbade10f857edd5a16bdc7aadf
Oliver Blanthorn <freedom4cows@gmail.com>
2020-02-27 18:51:42 +0000

    Make tests less fragile

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,13 @@ using JustJoshing
 using Test
 using ForwardDiff: derivative
 
+const EPS = 0.0001
+
 @testset "just_joshing.jl" begin
     # I: see code
 
     # II: Price of call option monotonically decreases with strike price
-    @test all(derivative.(x->C(1,0.5;K=x),rand(100_000)*10) .<= 0)
+    @test all(derivative.(x->C(1,0.5;K=x),rand(100_000)*10) .<= EPS)
 
     # III: Call option should be between S and S - Ke(-r(T-t)) for all inputs
     #
@@ -20,19 +22,19 @@ using ForwardDiff: derivative
     # NB: Joshi refers to time-to-maturity, i.e. t_joshi = T-t :)
     @test [
         let (S,t,K,r,σ) = rand(5), T = t + rand()
-            S >= C(S,t;T=T,K=K,r=r,σ=σ) >= (S - K*exp(-r*(T-t)))
+            S + EPS >= C(S,t;T=T,K=K,r=r,σ=σ) >= (S - K*exp(-r*(T-t))) - EPS
         end
     for _ in 1:100] |> all
 
     # IV: Price of call option should monotonically increase with volatility
-    @test all(derivative.(x->C(1,0.5;σ=x),rand(1000)) .>= 0)
+    @test all(derivative.(x->C(1,0.5;σ=x),rand(1000)) .>= -EPS)
 
     # V: if d=0 (always true in our model) then price should increase as we get closer to expiry
-    @test all(derivative.(T->C(1,0;T=T),rand(1000)) .>= 0)
+    @test all(derivative.(T->C(1,0;T=T),rand(1000)) .>= -EPS)
 
     # VI: Call option should be a convex function of strike - i.e. gradient is an increasing function of strike
     #   TODO: work out if there's a more ergonomic way of calculating higher order derivatives
-    @test all(derivative.(x->derivative.(K->C(1,0;K=K),x),rand(1000)) .>= 0)
+    @test all(derivative.(x->derivative.(K->C(1,0;K=K),x),rand(1000)) .>= -EPS)
 
     # VII: The price of a call-spread should approximate the price of a digital-call option
     # TODO - NOT IMPLEMENTED - 1) find out what call-spread is and price it; 2) find out to calculate price of digital call (it was really simple IIRC); 3) check they are roughly the same
