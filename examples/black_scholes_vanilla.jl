070df1436eba43e4b452a9b8d2bde03dd90df2ef
Oliver Blanthorn <freedom4cows@gmail.com>
2020-02-27 20:27:36 +0000

    Multithread MC pricer

--- examples/black_scholes_vanilla.jl
+++ examples/black_scholes_vanilla.jl
@@ -1,8 +1,10 @@
 #!/usr/bin/env julia
 
 # Best run in the REPL until I work out how to get unicodeplots to print to stdout when in an `include`
+# `env JULIA_NUM_THREADS=(nproc) julia --project`
 
 using JustJoshing
+using KissThreading
 
 import Plots
 Plots.unicodeplots()
@@ -19,8 +21,19 @@ Plots.plot(x->C(0.5,0;σ=x),0:0.01:1) # Call options with volatile underlyings a
 Plots.plot(x->B(x,σ=0.02),0:0.01:1)
 
 
-# Monte-Carlo validation of forward contract
+# Monte-Carlo validation of contracts from payoffs
 import Statistics: mean
 
-# TODO: multithread this
-mean([rand_price((S,K,t,T,r)->max(S-K,0),100;t=0.99,T=1,K=100,r=0.02,σ=0.05) for i in 1:100_000_000])
+#using BenchmarkTools
+
+
+# max(S-K,0): call option
+let S=100, t=0.99, K=100, r=0.02, σ=0.05, T=1, trials=100_000_000
+    bs = C(S,t;T=T,K=K,r=r,σ=σ)
+    (mean(rand_price((S,K,t,T,r)->max(S-K,0),S;t=t,T=T,K=K,r=r,σ=σ) for i in 1:trials) - bs)/ bs |> println
+
+    # Strictly we should use a thread-safe RNG here
+    ((tmapreduce(x->rand_price((S,K,t,T,r)->max(S-K,0),S;t=t,T=T,K=K,r=r,σ=σ),+,1:trials,init=0) / trials) -bs)/bs |> println
+
+    # 0.02% error roughly, good enough for me :)
+end
